9e89cd3558a43a118fbf28ae9a8aae8245f24e99
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-10-11 06:15:35 +0200

    Update files

---
+++ code/types/types.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from fractions import Fraction
+
+
+complex = 3 + 4j
+integer = 1234
+binary = 0b111
+dec = Decimal(binary)
+frac = Fraction(3, 4)
+
+print(complex)
+print(integer)
+print(binary)
+print(dec)
+print(frac)
